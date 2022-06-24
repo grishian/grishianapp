d50967a9d4dccffded722fcced71533542798fa3
grishian <grishian@hotmail.com>
2022-06-25 00:54:19 +0200

    added some logic

--- app.py
+++ app.py
@@ -1,14 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'my secret key'
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+class UserForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash('User added successfully!', 'OK')
+    our_users=Users.query.order_by(Users.date_added)
+
+    return render_template('add_user.html',
+                           form=form,
+                           name=name,
+                           our_users=our_users)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    first_name = 'John'
+    stuff = 'This is <strong>bold</strong> text.'
+    return render_template('index.html',
+                           first_name=first_name,
+                           stuff=stuff)
 
 
 @app.route('/user/<name>')
 def user(name):
-
-    return '<h1>Hello {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
