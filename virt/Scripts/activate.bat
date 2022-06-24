f55990f23dc9a9a7c34d5c84b4be230e90bac4dc
grishian <grishian@hotmail.com>
2022-06-24 23:07:03 +0200

    did some stuff

--- virt/Scripts/activate.bat
+++ virt/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\grish\Documents\eigen_programmeer_projecten\grishianapp\virt
+set VIRTUAL_ENV=C:\Users\grish\Documents\eigen_programmeer_projecten\grishianapp1\virt
 
 if not defined PROMPT set PROMPT=$P$G
 
