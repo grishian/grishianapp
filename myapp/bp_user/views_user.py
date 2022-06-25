c5205507f0991f93c6fc4005c54c7fad002f0fc9
grishian <grishian@hotmail.com>
2022-06-25 19:50:47 +0200

    changed user list

--- myapp/bp_user/views_user.py
+++ myapp/bp_user/views_user.py
@@ -7,22 +7,29 @@ from myapp.bp_user.model_user import User
 
 @bp_user.route('/user/<name>')
 def do_user(name):
-    return render_template('user.html', name=name)
+    return render_template('user/user.html', name=name)
 
 
 @bp_user.route('/register', methods=['GET', 'POST'])
 def do_register():
-
     if request.method == 'POST':
         username = request.form.get('input_username')
+        password = request.form.get('input_password')
+        # password_check = request.form.get('input_password_check')
 
         user = User()
         user.username = username
+        user.set_password(password)
+        user.active = True
+        user.profile_type = 1
 
         db.session.add(user)
         db.session.commit()
 
+    return render_template('user/register.html')
 
 
-
-    return render_template('register.html')
+@bp_user.route('/user_list')
+def do_user_list():
+    users = User.query.all()
+    return render_template('user/user_list.html', users=users)
