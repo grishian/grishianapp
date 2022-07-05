94859562f873b4abe69f699714bebc823b306094
grishian <grishian@hotmail.com>
2022-07-05 16:38:53 +0200

    url typo

--- configuration.py
+++ configuration.py
@@ -3,7 +3,7 @@ class BaseConfiguration():
     SECRET_KEY = 'my secret key'
 
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
-    SQLALCHEMY_DATABASE_URI = 'postgres://ppcgnjbgfpjuzr:d74db855ab3944cd27385f1c94fe43c164582840632c02f78deceb9650f8bb3c@ec2-3-226-163-72.compute-1.amazonaws.com:5432/d82t048gtm2ur9'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://ppcgnjbgfpjuzr:d74db855ab3944cd27385f1c94fe43c164582840632c02f78deceb9650f8bb3c@ec2-3-226-163-72.compute-1.amazonaws.com:5432/d82t048gtm2ur9'
     DEBUG_TB_ENABLED = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
